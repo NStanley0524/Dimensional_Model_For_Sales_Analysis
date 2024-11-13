4db3a58e0cc29bc0f3318fadb67cd5bf18594c85
Nwajagu Stanley <nwajagustanley@gmail.com>
2024-11-13 15:16:08 +0100

    Update Table_creation_script.sql

--- Code_Scripts/Table_creation_script.sql
+++ Code_Scripts/Table_creation_script.sql
@@ -99,4 +99,29 @@ select * from Invoice;
 select * from Job;
 select * from Parts;
 
+insert into Vehicle
+values
+(2, "Toyota", "Corolla", 2015, "White", "TYS678901234567-876Z", "TOY 456", 45000, "Michael Smith", 4),
+(3, "Honda", "Civic", 2018, "Blue", "HCS345678901234-123X", "HON 789", 30000, "Sarah Johnson", 6),
+(4, "Ford", "Escape", 2020, "Red", "FES234567890123-456Y", "FOR 987", 15000, "Emily Brown", 6),
+(5, "Chevrolet", "Malibu", 2016, "Silver", "CMS456789012345-789Z", "CHE 321", 60000, "David Wilson", 6);
 
+-- updated mistakes 
+
+update Job
+set invoice_id = 12349 where JobID = 11;
+
+update Invoice
+set Vehicle_id = 5
+where invoice_id = 12349;
+
+insert into Customer
+values
+(2, "Michael Smith", "250 Broadway, Winnipeg, MB, R3C 0R5", "204-555-1234", 4, 2),
+(3, "Sarah Johnson", "789 Main St, Winnipeg, MB, R2W 3N2", "204-666-5678", 6, 3),
+(4, "Emily Brown", "456 Elm St, Winnipeg, MB, R3M 2S5", "2204-777-9101", 8, 4),
+(5, "David Wilson", "123 Oak St, Winnipeg, MB, R2J 3C4", "204-888-1112", 10, 5);
+
+create index C_PartID_idx on Customer(PartID);
+create index C_VehicleID_idx on Customer(Vehicle_id);
+create index V_PartID_idx on Vehicle(PartID);
